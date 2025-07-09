9a8ed42cf9b64f4c7d4aec1fa55ea5d2cd3385e9
Arun Vijo Tharakan <arunvijo2004@gmail.com>
2025-07-09 22:54:46 +0530

    Bug: updated speech logic

--- utils/speech_utils.py
+++ utils/speech_utils.py
@@ -1,7 +1,17 @@
-import pyttsx3
+try:
+    import pyttsx3
+    engine = pyttsx3.init()
+    tts_enabled = True
+except Exception:
+    print("[TTS DISABLED] pyttsx3 not supported in this environment.")
+    engine = None
+    tts_enabled = False
 
 def speak_token(token, name):
-    engine = pyttsx3.init()
     msg = f"Token number {token}, patient {name}, please proceed."
-    engine.say(msg)
-    engine.runAndWait()
+    if tts_enabled and engine:
+        engine.say(msg)
+        engine.runAndWait()
+    else:
+        print(f"[AUDIO] {msg}")
+
